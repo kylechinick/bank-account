09eac0c77bd7905b182f46a2d8493ee575eae6e2
Kyle Chinick <kylechinick@icloud.com>
2022-06-14 09:58:40 -0700

    Add BankAccount and Bank constructors, deposit(), withdrawal(), and and assignId() prototypes
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,48 @@
-//Business Logic 
-function
+//Business Logic
+function BankAccount(name, ballance) {
+  this.name = name;
+  this.ballance = ballance;
+}
 
-/* constructor for account
+function Bank() {
+  this.bankAccounts = {};
+  this.currentId = 1000;
+}
 
-ballance
-account name
-account number
+//Bank Functions
 
-prototype withdraw
-prototype deposit
+Bank.prototype.newAccount = function (BankAccount) {
+  this.BankAccount = this.assignId();
+  this.bankAccounts[BankAccount.id] = BankAccount;
+};
 
-if done make a bank
+Bank.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+//Account Functions
+BankAccount.prototype.deposit = function (amount) {
+  this.ballance += amount;
+  return this.ballance;
+};
+
+BankAccount.prototype.withdrawal = function (amount) {
+  this.ballance -= amount;
+  return this.ballance;
+};
+
+// * set currentId 1000
+
+// BUILD REQUIREMENTS:
+
+// --> constructor for account
+
+// --> ballance
+// --> account name
+// --> account number
+
+// --> prototype withdraw
+// --> prototype deposit
+
+// if done make a bank`
