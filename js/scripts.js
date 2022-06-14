9cfbf3163d2ccec3700a554c78d13e85df6c7281
Kyle Chinick <kylechinick@icloud.com>
2022-06-14 14:06:08 -0700

    WIP push current work to aid in troubleshooting the value of an object's balance
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,9 @@ function Bank() {
 
 //Bank Functions
 
-Bank.prototype.newAccount = function (BankAccount) {
-  this.BankAccount = this.assignId();
-  this.bankAccounts[BankAccount.id] = BankAccount;
+Bank.prototype.newAccount = function (bankAccount) {
+  bankAccount.id = this.assignId();
+  this.bankAccounts[bankAccount.id] = bankAccount;
 };
 
 Bank.prototype.assignId = function () {
@@ -23,7 +23,10 @@ Bank.prototype.assignId = function () {
 
 //Account Functions
 BankAccount.prototype.deposit = function (amount) {
+  console.log('Amount to deposit: ', amount);
+  console.log('Starting balance: ', this.ballance);
   this.ballance += amount;
+  console.log('Updated balance: ', this.ballance);
   return this.ballance;
 };
 
@@ -31,6 +34,64 @@ BankAccount.prototype.withdrawal = function (amount) {
   this.ballance -= amount;
   return this.ballance;
 };
+
+// let bankAccountOne = new BankAccount('Cool Name Dude', 3.5);
+// console.log(bankAccountOne);
+
+// bank.newAccount(bankAccountOne);
+
+// UI Logic
+
+// STARTER PACK:
+
+let bank = new Bank();
+console.log(bank);
+
+let newAccountObject = new BankAccount('test account', 20);
+bank.newAccount(newAccountObject);
+
+console.log(bank);
+
+$(document).ready(function () {
+  $('#new-account-form').submit(function (event) {
+    event.preventDefault();
+    const newAccountName = $('#new-account-name-field').val();
+    const initialDepositAmount = parseInt($('#initial-deposit-field').val());
+
+    let newAccountObject = new BankAccount(
+      newAccountName,
+      initialDepositAmount
+    );
+    bank.newAccount(newAccountObject);
+    newAccountObject.deposit(initialDepositAmount);
+  });
+
+  $('#update-balance-form').submit(function (event) {
+    event.preventDefault();
+    const depositAmount = parseInt($('#deposit-field').val());
+    // console.log(depositAmount);
+    const withdrawalAmount = parseInt($('#withdrawal-field').val());
+
+    // let tempOnlyAccount = bank.bankAccounts[1001];
+
+    tempOnlyAccount.deposit(depositAmount);
+
+    console.log('Test account balance:', bank.bankAccounts[1001]);
+    console.log(typeof bank.bankAccounts[1001].ballance);
+
+    tempOnlyAccount.withdrawal(withdrawalAmount);
+  });
+});
+
+// bankAccountOne.deposit(100);
+// console.log(bankAccountOne);
+
+// bankAccountOne.withdrawal(25);
+// console.log(bankAccountOne);
+
+// bank.newAccount(bankAccountOne);
+// console.log(bankAccountOne);
+// console.log(bank);
 
 // * set currentId 1000
 
